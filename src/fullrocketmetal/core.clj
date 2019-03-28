a93b79a4e6857c2324fb3ad0dae7b4d0bd65f8c8
dmaiocchi <dmaiocchi@suse.com>
2019-03-29 00:14:05 +0100

    add servername

--- src/fullrocketmetal/core.clj
+++ src/fullrocketmetal/core.clj
@@ -14,8 +14,13 @@
     (-> config-map :credentials :rocketchat :password))
   
 
+(def get-rocket-server
+      (-> config-map :credentials :rocketchat :servername))
+    
+
+
 (defn -main
   [& args]
-  (let [response  (rocket/init-client get-rocket-user get-rocket-password)]
+  (let [response  (rocket/init-client get-rocket-server get-rocket-user get-rocket-password)]
   (println "response1's status: " (:status @response))
   ))
--- src/fullrocketmetal/http.clj
+++ src/fullrocketmetal/http.clj
@@ -1,5 +1,5 @@
 (ns fullrocketmetal.http
     (:require [clj-http.client :as client]))
 
-(defn init-client [user pwd]
-    (client/get "https://chat.suse.de/api/v1/login" {:basic-auth [user pwd ] :insecure? true} ))
+(defn init-client [server user pwd]
+    (client/get server {:basic-auth [user pwd ] :insecure? true} ))
