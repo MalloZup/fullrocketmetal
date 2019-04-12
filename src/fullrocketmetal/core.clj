6d8fd53e11ae94b3ae29cb13fc52577ee4d89026
dmaiocchi <dmaiocchi@suse.com>
2019-04-12 14:15:19 +0200

    implement send-reminder

--- src/fullrocketmetal/core.clj
+++ src/fullrocketmetal/core.clj
@@ -10,19 +10,41 @@
 (clojure.edn/read-string (slurp "events.edn")))
 
 (defn init-rocketchat-client  []
-  (config/set-config-from-file ".rocketchat.edn")
-  (println (channels/list!)))
+  (config/set-config-from-file ".rocketchat.edn"))
   ;; read channels name, message etc.
   ;; iterate here over a list of channelsf
   ;; (chat/sendMessage channnel-id message))
 
+(defn get-reminders []
+  (get-in (events) [:reminders]))
 
 (defn send-reminder []
   "dispatch message/reminder to rocketchat based on configuration"
-   (println "WIP"))
+  (init-rocketchat-client)
+  
+  ;; assemble channel-id and message so we can send it later
+  (let [channel-id-and-message
+        (interleave 
+  ;; get channel-id as sequence  
+         (map (fn [channel-name] (channels/get-channel-id channel-name)) (map :channel-name (get-reminders)))
+  ;; get all messages as sequence
+         (map :message (get-reminders))
+         )]
+    
+  ;; TODO: setup the time logic for scheduling message at given time. ( before 5 min etc)
+    
+    
+  ;; TODO: would be nice to send this messages in parallel (futures)
+  ;; send message to the channel name given
+    (map (fn [channel-id message] (chat/sendMessage channel-id message)) channel-id-and-message)))
+  ;;(chat/sendMessage channel-id message)
 
+ 
+  ;; TODO: in the main we should act like a daemon, 
+  ;; 1) refresh/re-read configuration if something has changed
+  ;; 2) check
+  ;; 3) print debug infos ( rate-limiting)
   (defn -main []
     ;; read reminders map configuration
-    (apply send-reminder (get-in (events) [:reminders]))
-  
+    (send-reminder)
     (println "main"))
