56cf1e94c0b36a07a46b89870cc3a9c67b0d160e
dmaiocchi <dmaiocchi@suse.com>
2019-04-16 19:00:43 +0200

    use quarzite

--- src/fullrocketmetal/core.clj
+++ src/fullrocketmetal/core.clj
@@ -1,34 +1,22 @@
 (ns fullrocketmetal.core
   (:require [missile.channels :as channels]
             [missile.config :as config]
-            [timely.core :as timely]
             [fullrocketmetal.reminders :as reminders]
             [missile.chat :as chat])
    (:gen-class))
-
+  
 (defn init-rocketchat-client  []
   (config/set-config-from-file ".rocketchat.edn"))
-
-(defn test-print-fn []
-  (fn []
-    (println "Task scheduled at: " (java.util.Date.))))
 
 ;; we might call this in future with core.async for moment is ok.
 (defn daemonize []
   (while true
     ;; todo watch if a file changed (conf) and perfom some operation
-    
-    ;; perform some operation
-    ;; TODO add task to scheduler using timely.
-
-    ;; sleep 1 min
-    (let [interval (* 1 60 1000)]
+    ;; sleep 5 min
+    (let [interval (* 2 60 1000)]
       (Thread/sleep interval))
-  
-    ;; print debug infos
-    ;; TODO
-
-    (println "sleeping 1 min")
+    (println "sleeping 2 min")
+    ;; print debug infos  ;; TODO
     (flush)
 ))
   ;; TODO: setup the time logic for scheduling message at given time. ( before 5 min etc)
@@ -39,4 +27,6 @@
   ;; 3) print debug infos ( rate-limiting)
 (defn -main []
   (init-rocketchat-client) 
+
+  (daemonize)
 )
