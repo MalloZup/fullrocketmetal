b9c2de56c9f92828f77590459931758098d5278d
dmaiocchi <dmaiocchi@suse.com>
2019-04-26 19:52:59 +0200

    prepare experimental version for scheduler

--- src/fullrocketmetal/reminders.clj
+++ src/fullrocketmetal/reminders.clj
@@ -1,17 +1,7 @@
-(ns fullrocketmetal.reminders
-  (:require [missile.channels :as channels]
-            [missile.chat :as chat]))
+(ns fullrocketmetal.reminders)
 
 (defn events []
   (clojure.edn/read-string (slurp "events.edn")))
 
 (defn get-reminders []
   (get-in (events) [:reminders]))
-
-(defn send-single-reminder [reminder-map]
-  (chat/sendMessage  (missile.channels/get-channel-id (:channel-name reminder-map)) (:message reminder-map)))
-
-;; send all messages
-(defn send-reminders []
-  "dispatch message/reminder to rocketchat based on configuration"
-  (map send-single-reminder (get-reminders)))
--- src/fullrocketmetal/scheduler.clj
+++ src/fullrocketmetal/scheduler.clj
@@ -2,15 +2,27 @@
   (:require
             [clojurewerkz.quartzite.triggers :as t]
             [clojurewerkz.quartzite.jobs :as j]
+            [clojurewerkz.quartzite.conversion :as qc]
             [clojurewerkz.quartzite.jobs :refer [defjob]]
             [clojurewerkz.quartzite.schedule.cron :as qcron]
-            [missile.chat :as chat])
+            [missile.chat :as chat]
+            [missile.channels :as channels])
    (:gen-class))
 
-(defn create-rocket-msg-cron-trigger []
+(defn create-rocket-msg-cron-trigger [cron-schedule-time]
   (t/build
-    ;; TODO: think later on UID if needed
-    ;; (t/with-identity (t/key "triggers.1"))
-    (t/start-now)
-    (t/with-schedule (qcron/schedule  (qcron/cron-schedule "0 0/1 * * * ?"))))) 
+  (t/start-now)
+   (t/with-schedule (qcron/schedule (qcron/cron-schedule cron-schedule-time)))))
 
+(defjob rocket-message-job  [ctx]
+  (let [data (qc/from-job-data ctx)
+        ch-name (data "ch-name")
+        text-message (data "text-message")]
+    ;; (chat/sendMessage  (channels/get-channel-id ch-name) text-message)
+    (println ch-name)
+    (println text-message)))
+
+(defn create-rocket-msg-job [list-jobs]
+  (j/build
+    (j/of-type rocket-message-job)
+    (j/using-job-data {"ch-name" (:channel-name list-jobs) "text-message" (:message list-jobs)})))
